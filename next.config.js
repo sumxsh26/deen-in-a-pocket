76d77a0ba810e0e3ac441efd79e5c79bcd66cb42
sumxsh26 <smyyhzaii@gmail.com>
2025-11-21 11:01:04 +0800

    init bismillah

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** next.config.js */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: { unoptimized: false } // tweak if using external image domains
+};
+module.exports = nextConfig;
