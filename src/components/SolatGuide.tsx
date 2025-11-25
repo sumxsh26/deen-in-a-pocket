e1813cbbdaf1f6c5fcf30e72ef8adf1ea6165cf4
sumxsh26 <smyyhzaii@gmail.com>
2025-11-26 00:53:45 +0800

    audio check

--- src/components/SolatGuide.tsx
+++ src/components/SolatGuide.tsx
@@ -31,7 +31,7 @@ const solatSteps = [
     meaning: 'In the name of Allah, the Most Gracious, the Most Merciful. All praise is for Allah—Lord of all worlds, the Most Compassionate, Most Merciful, Master of the Day of Judgment. You ˹alone˺ we worship and You ˹alone˺ we ask for help. Guide us along the Straight Path, the Path of those You have blessed—not those You are displeased with, or those who are astray.',
     description: 'Place your right hand over your left on your chest. Recite Surah Al-Fatihah in a clear, moderate voice.',
     posture: <img src="/images/2.png"/>,
-    audio: "/audio/Audio1.mp3"
+    audio: "/audio/AlFatihah.mp3"
   },
   {
     step: 3,
